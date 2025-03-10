707b2ceafd9a7f61a429f8729991111fa2d158e1
ycz <ycz_20011201@163.com>
2025-03-10 15:52:32 +0800

    add mapper test

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,20 @@
+package com.xuecheng;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ycz
+ * @version 1.0
+ * @description 内容管理服务启动类
+ * @date 2025/3/5
+ */
+
+
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseMapperTests.java
@@ -0,0 +1,22 @@
+package com.xuecheng.content;
+
+
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CourseBaseMapperTests {
+
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+    @Test
+    public void testCourseBaseMapper(){
+        CourseBase courseBase = courseBaseMapper.selectById(18L);
+        Assertions.assertNotNull(courseBase);  //junit中的方法，断言一下对象是否为空，如果为空，那么测试就会抛异常
+    }
+}
