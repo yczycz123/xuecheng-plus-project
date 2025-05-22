1bb11bde424e2e6d52b6810f37318509d2d24520
ycz <ycz_20011201@163.com>
2025-05-22 18:52:56 +0800

    add some new features.

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
@@ -1,8 +1,10 @@
 package com.xuecheng.content.service;
 
 
+import com.xuecheng.content.model.dto.BindTeachplanMediaDto;
 import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.TeachplanMedia;
 
 import java.util.List;
 
@@ -10,17 +12,30 @@ import java.util.List;
 //课程计划管理相关接口
 public interface TeachplanService {
 
+
     /**
      * 根据课程id查询课程计划
+     *
      * @param courseId 课程id
-     * @return  课程计划
+     * @return 课程计划
      */
     public List<TeachplanDto> findTeachplanTree(long courseId);
 
 
     /**
      * 新增/保存/修改课程计划
+     *
      * @param teachplanDto
      */
     public void saveTeachplan(SaveTeachplanDto teachplanDto);
+
+
+    /**
+     * 教学计划绑定媒资
+     *
+     * @param bindTeachplanMediaDto
+     * @return com.xuecheng.content.model.po.TeachplanMedia
+     */
+    public TeachplanMedia associationMedia(BindTeachplanMediaDto bindTeachplanMediaDto);
+
 }
