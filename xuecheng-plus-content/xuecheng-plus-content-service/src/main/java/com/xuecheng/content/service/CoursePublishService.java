d33cd59b70f05cc100e9cdf1704bfccf62225e50
ycz <ycz_20011201@163.com>
2025-05-23 21:46:06 +0800

    add some new features.

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -0,0 +1,35 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.dto.CoursePreviewDto;
+
+/**
+ * @description 课程预览、发布接口
+ * @author Mr.M
+ * @date 2022/9/16 14:59
+ * @version 1.0
+ */
+public interface CoursePublishService {
+
+
+    /**
+     * @description 获取课程预览信息
+     * @param courseId 课程id
+     * @return com.xuecheng.content.model.dto.CoursePreviewDto
+     * @author Mr.M
+     * @date 2022/9/16 15:36
+     */
+    public CoursePreviewDto getCoursePreviewInfo(Long courseId);
+
+
+
+    /**
+     * @description 提交审核
+     * @param courseId  课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/18 10:31
+     */
+    public void commitAudit(Long companyId,Long courseId);
+
+
+}
